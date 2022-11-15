3660e9f5bd762c1fd1b8dd92245c1ef8f5a00094
Jeanne Olsen <Jeanne.Olsen@my.strayer.edu>
2022-11-15 02:00:21 -0600

    Add project files.

---
+++ week_6_lab_2.py
@@ -0,0 +1,43 @@
+#Jeanne Olsen
+#CIS 261 Week 6 Lab 1
+
+def get_price():
+    while True:
+        try:
+            price = float(input("Enter price: "))
+            return price
+        except ValueError:
+            print("Invalid decimal number. Please try again.")
+
+def get_quantity():
+    while True:
+        try:
+            quantity = int(input("Enter quantity: "))
+            return quantity
+        except ValueError:
+            print("Invalid integer. Please try again.")
+
+def main():
+    print("The Invoice Line Item Calculator\n")
+
+    answer = "y"
+    while answer.lower() == "y":
+        # get the price and quantity
+        price = get_price()
+        quantity = get_quantity()
+    
+        # calculate the total
+        total = price * quantity
+
+        # display the results
+        print()
+        print("PRICE:    ", f"{price: .2f}")
+        print("QUANTITY: ", quantity)
+        print("TOTAL:    ", f"{total: .2f}")
+        answer = input("Enter another line item? (y/n): ")
+        print()
+        
+    print("Bye!")
+
+
+if __name__ == "__main__":
